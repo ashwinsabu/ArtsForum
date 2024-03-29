eef62cb2591b0e8742deb7e448b3313cae15ce8e
ashwin <x23196505@student.ncirl.ie>
2024-03-29 15:45:43 +0000

    final corrections

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import boto3
 import json
 from pathlib import Path
+import boto3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
