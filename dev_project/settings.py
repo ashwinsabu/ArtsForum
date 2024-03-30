1697f88a98de0311c572521170c5bd04d3e51174
ashwin <x23196505@student.ncirl.ie>
2024-03-30 11:08:21 +0000

    eb error

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = data['django']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['x23196505DevopsProjectEnv.eba-mncdgksn.eu-west-1.elasticbeanstalk.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['https://x23196505DevopsProjectEnv.eba-mncdgksn.eu-west-1.elasticbeanstalk.com']
 
 # Application definition
 
